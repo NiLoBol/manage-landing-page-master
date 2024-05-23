f4d039d06e7d06d1ffd480cfb44d64d067b8356a
Bruno Moleta <brunomoleta@pm.me>
2024-05-23 13:01:43 -0300

    :recycle: refactor:Change <main> architecture

---
+++ src/components/main/AllTexts.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="basis-1/2 max-lg:basis-full my-10">
+    <Managetext
+      v-for="({ text, title }, index) in items"
+      :head="title"
+      :key="index"
+      :id="`0${index + 1}`"
+    >
+      {{ text }}
+    </Managetext>
+  </div>
+</template>
+
+<script setup>
+import Managetext from "@/components/main/Managetext.vue";
+import { reactive } from "vue";
+import data from "@/data/appFeatures.json";
+
+const items = reactive(data);
+</script>
