8f4c6525403007081ffd76b02bd385aa5458f7ac
NiLoBol <sathaporn733@gmail.com>
2024-05-23 14:57:05 +0700

    vue3

---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template lang="">
+  <a
+    v-if="!condition"
+    :class="[
+      'lg:text-lg text-sm font-semi bg-BrightRed text-white p-3 rounded-full px-6',
+      { 'hover:cursor-pointer': pointer }
+    ]"
+  >
+    <slot></slot>
+  </a>
+  <a
+    v-else
+    className="lg:text-lg text-sm bg-white text-BrightRed font-bold p-3 rounded-full px-6 hover:cursor-pointer"
+  >
+    <slot></slot>
+  </a>
+</template>
+<script>
+export default {
+  name: "Button",
+  props: { condition: Boolean, pointer: Boolean },
+};
+</script>
+<style lang=""></style>
---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "img":"url('./src/images/bg-tablet-pattern.svg')",
+
+        "sd":"url('./src/images/bg-simplify-section-desktop.svg')",
+        "sm":"url('./src/images/bg-simplify-section-mobile.svg')"
+      },
+      fontWeight: {
+        bold: "700",
+        semi: "500",
+        normal: "400",
+      },
+      colors: {
+        BrightRed: "hsl(12, 88%, 59%)",
+        DarkBlue: "hsl(228, 39%, 23%)",
+        DarkGrayishBlue: "hsl(227, 12%, 61%)",
+        VeryDarkBlue: "hsl(233, 12%, 13%)",
+        VeryPaleRed: "hsl(13, 100%, 96%)",
+        VeryLightGray: "hsl(0, 0%, 98%)",
+      },
+      fontSize:{
+        sub:""
+      },
+      
+      
+    },
+    
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
